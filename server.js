68ba8314bcb184ba1289e07e85dd3087bc2fc968
Cody T <crthorn11@gmail.com>
2024-03-31 00:18:45 -0400

    finished creating some more paths and routes for now

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express.js');
+const express = require('express');
 const fs = require('fs');
 const api = require('./routes/index.js');
 const path = require('path');
@@ -6,6 +6,30 @@ const path = require('path');
 const PORT = 3001;
 
 const app = express();
+
+const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.js'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 
 app.get('/', (req, res) => {
     res.send(
@@ -31,3 +55,5 @@ console.log(`Example app listening at http://localhost:${PORT}`)
 //get /notes to return notes.html file
 //use previous line to add it to db.json body
 //research npm packages on saving the notes
+//add function to delete notes
+//Support@heroku.com
